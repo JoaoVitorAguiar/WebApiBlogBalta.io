1112d442cf3e155ebe66b33f3053e1c85b9495e5
jvAguiarSilva <107854662+jvAguiarSilva@users.noreply.github.com>
2023-07-23 14:58:04 -0300

    api key

--- Configuration.cs
+++ Configuration.cs
@@ -2,5 +2,8 @@
 public class Configuration
 {
     // Token - JWT - Json Web Token
-    public static string JwtKey { get; set; } = "7Ls3qs9OXp0psOSuff2w528bp4qKpC20";
+    public static string JwtKey = "7Ls3qs9OXp0psOSuff2w528bp4qKpC20";
+    // Sempre que passar este parâmetro na requisição ele será autenticado
+    public static string ApiKeyName = "api_key";
+    public static string ApiKey = "haligdauigfuweluieaeggdlslgeiulggf";
 }
